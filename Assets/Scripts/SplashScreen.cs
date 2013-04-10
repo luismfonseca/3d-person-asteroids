18c7d3c9ee807745cea03d3004d3f53b30722c85
OmarCastro <omar.castro.3600@gmail.com>
2013-04-10 14:40:38 +0100

    fixed fonts

--- Assets/Scripts/SplashScreen.cs
+++ Assets/Scripts/SplashScreen.cs
@@ -5,11 +5,16 @@ public class SplashScreen : MonoBehaviour{
 	public float timer =4f;
 	public string levelToLoad="start";
 
-	
+	public GUIStyle style = new GUIStyle();
 	//start
-
+	
 	void Start(){
+        style.normal.textColor = new Color(0, 0, 0);
+        style.hover.textColor = new Color(0f, 256f, 256f);
 
+        //style.font = (Font)Resources.Load("Vector Battle");
+		style.fontSize = 20;
+		
 		StartCoroutine ("DisplayScene"); 
 	}
 
@@ -17,6 +22,22 @@ public class SplashScreen : MonoBehaviour{
 		 yield return new WaitForSeconds(timer);
 		Application.LoadLevel(levelToLoad);
 
+	}
+	
+	void OnGUI(){
+		int width = Screen.width;
+		int height = Screen.height;
+		Rect labelRect = new Rect (width/2, (height/4), 0, (height/2));
+		
+		string text = 
+			@"Omar Alejandro Castillo de Castro
+Luís Miguel Guimarães Pimentel Fonseca
+Witold Zgrabka
+
+presents:";
+		
+		GUI.Label(labelRect,text,style);
+		
 	}
 	
 	
