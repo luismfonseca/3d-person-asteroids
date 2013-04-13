f77353bc070b8478a757df3e7c9078760d8898c2
OmarCastro <omar.castro.3600@gmail.com>
2013-04-13 21:12:46 +0100

    deleted old explosion, removed unused scripts

--- Assets/Scripts/Start.js
+++
@@ -1,8 +0,0 @@
-function OnGUI () {
-
-    if (GUI.Button (Rect ((Screen.width/2-60), (Screen.height/2)+80, 120, 30), "START GAME")) {
-
-        Application.LoadLevel("level1");
-
-    }
-}
--- Assets/Scripts/TitleGUI.js
+++
@@ -1,9 +0,0 @@
-#pragma strict
-function OnGUI()
-{
-	if(GUI.Button(Rect(Screen.width/2 - 120/2,Screen.height/2+80,120,30),"Start Game"))
-	{
-
-
-	}
-}
